19e0e27d60f1eae3b0c981904b8977a0a70d7cd3
Hermann Käser <hermann.kaser@gmail.com>
2018-11-30 10:01:36 -0500

    Adds i3 and polybar configs

---
+++ i3/scripts/4k-display.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-2 --off
+~/.config/i3/scripts/set-background.sh
---
+++ i3/scripts/set-background.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+BACKGROUND=/home/hermzz/Pictures/20150906_084029-2.jpg
+
+/usr/bin/feh --bg-fill $BACKGROUND
+betterlockscreen -u $BACKGROUND
