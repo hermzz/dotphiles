a0a83bd26df853a946da1897124b633ef0868d3d
Hermann Käser <hermann.kaser@gmail.com>
2020-10-02 15:09:54 -0400

    Use picom compositor, polybar updates

--- i3/scripts/4k-display.sh
+++ i3/scripts/4k-display.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output DP1 --mode 3840x2160 --pos 0x0 --rotate normal --output eDP1 --off
+xrandr --output DP2 --mode 3840x2160 --pos 0x0 --rotate normal --output eDP1 --off
 ~/.config/i3/scripts/set-background.sh
